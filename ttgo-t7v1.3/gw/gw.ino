1fa420d733adc2939c7c2e74ad6b53c7e963c26f
zetxx <zetxx@belogradchik.biz>
2022-12-11 17:10:26 +0200

    mysensors

--- ttgo-t7v1.3/gw/gw.ino
+++ ttgo-t7v1.3/gw/gw.ino
@@ -17,24 +17,25 @@
 #define MY_RADIO_RF24
 #define MY_RF24_CS_PIN 5
 #define MY_RF24_CE_PIN 25
+#define MY_RF24_PA_LEVEL RF24_PA_HIGH
 
 #define MY_GATEWAY_SERIAL
 #define MY_GATEWAY_ESP32
-#define MY_HOSTNAME "ESP32MQTTGW"
+#define MY_HOSTNAME "mysensors-gw-selqni"
 
 #define MY_GATEWAY_MQTT_CLIENT
-#define MY_MQTT_PUBLISH_TOPIC_PREFIX "mygateway1-out"
-#define MY_MQTT_SUBSCRIBE_TOPIC_PREFIX "mygateway1-in"
-#define MY_MQTT_CLIENT_ID "mysensors-1"
+#define MY_MQTT_PUBLISH_TOPIC_PREFIX "selqni-gateway1-out"
+#define MY_MQTT_SUBSCRIBE_TOPIC_PREFIX "selqni-gateway1-in"
+#define MY_MQTT_CLIENT_ID "mysensors-selqni"
 #define MY_PORT 1883
 #define MY_CONTROLLER_IP_ADDRESS 10,100,100,22
 
-#define MY_DEBUG
-#define MY_DEBUG_VERBOSE
-#define MY_DEBUG_VERBOSE_GATEWAY
-#define MY_DEBUG_VERBOSE_SIGNING
-#define MY_DEBUG_VERBOSE_RF24
-#define MY_DEBUG_VERBOSE_TRANSPORT
+// #define MY_DEBUG
+// #define MY_DEBUG_VERBOSE
+// #define MY_DEBUG_VERBOSE_GATEWAY
+// #define MY_DEBUG_VERBOSE_SIGNING
+// #define MY_DEBUG_VERBOSE_RF24
+// #define MY_DEBUG_VERBOSE_TRANSPORT
 
 
 #define MY_SIGNING_SOFT
